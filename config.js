b13113d5d839e8bfdf14c6f759e9e55dacf74ea3
karynproject542 <njaykeren7@gmail.com>
2025-09-15 00:30:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+//~~~~~~~~~ Setting Owner~~~~~~~~~~//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = [""]
+global.namabot = 'JUSTIN X SATANIC'
+//~~~~~~~~~ Setting ~~~~~~~~~~//
+global.namach = "Justin|Satanic Kite Fighter!!"
+global.link = "https://whatsapp.com/channel/0029Vavn9WRJpe8p3EYLrw16"
+global.idch = "120363346890803631@newsletter"
+global.imgthumb = "https://files.catbox.moe/tdcmj7.jpg"
+global.vidthumb = "https://files.catbox.moe/ju0qq0.mp4"
+//~~~~~~~~~ Setting Mess ~~~~~~~~~~
+global.mess = { 
+owner: 'Akses Ditolak, Number Tidak Terdaftar Sebagai Owner!!\n\n> Add Pengguna Sebelum Akses, Atau Ganti Nomer Owner Di Bagian Config.js Agar Script Bisa Di Akses!!',
+premium: 'Akses DitolakNumber Tidak Terdaftar Sebagai Premium!!\n\n> Add Pengguna Sebelum Akses, Atau Ganti Nomer Owner Di Bagian Config.js Agar Script Bisa Di Akses!!',
+succes: 'Akses Ditrima!! Selamat Menikmati Bug JustinXSatanic V1 Free Only ( Special 12Rb Pengikut )'
+//~~~~~~~~~ End ~~~~~~~~~~//
+}
